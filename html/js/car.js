b0cbefca2528f4136edf33371906367036e32b40
anhhuynh1998 <anhprono1998@gmail.com>
2023-03-10 15:55:41 +0700

    create info file

---
+++ html/js/car.js
@@ -0,0 +1,10 @@
+function Rigth(){
+    if(document.getElementById('r').innerText == 'Rigth'){
+        document.getElementsByClassName('car')[0].scr = 'img/Untilted.png';
+    }
+}
+function Left(){
+    if(document.getElementById('l').innerText == 'Left'){
+        document.getElementsByClassName(car)[0].scr = 'img/Untilted1.png';
+    }
+}
