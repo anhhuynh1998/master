af989cd6cab76908226f0ab1d3143809d9856bb9
anhhuynh1998 <anhprono1998@gmail.com>
2023-03-11 20:46:29 +0700

    create info file

---
+++ html/js/click.js
@@ -0,0 +1,8 @@
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+    console.log('Xin chào!');
+
+    document.write('Xin chàoo');
+
+    alert('xin chào')
+}
